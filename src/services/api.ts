a2cfa7655f975dcd950c32588d346a071520b7ce
Daniel Rocha <125128961+IFullStackDanielRocha@users.noreply.github.com>
2024-03-25 00:14:04 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const api= axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params:{
-        api_key:'986e036fec76e6eaf2c818cb98f79e71',
+        api_key:'',
         language: "pt-BR",
         include_adults: true,
 
